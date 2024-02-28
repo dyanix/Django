53183d5bc71cec34bce68c4174418ffaa1ac153c
dyanix <dyanixdhawale@gmail.com>
2024-02-28 23:00:14 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 function App() {
+  const [students, setStudents] = useState([])
+
+  const fetchstudents = async () => {
+
+    try {
+      const students = await axios.get("http://127.0.0.1:8000/api/student/")
+      console.log(students.data)
+
+      setStudents(students.data)
+
+
+    } catch (error) {
+      console.log(error)
+
+    }
+  }
+  useEffect(() => {
+    fetchstudents()
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      {
+        students.map((student) => {
+          return (
+            <>
+              <h1>{student.stuname}</h1>
+              <h1>{student.email}</h1>
+            </>
+
+          )
+        })
+      }
+
+
     </div>
   );
 }
